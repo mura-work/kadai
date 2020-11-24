e060d53b44d16a00f45a8b1bb94103d6fbced024
murata-naoya <murata.small.baseball@gmail.com>
2020-11-24 10:59:16 +0000

    finish[kadai9]

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
 
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
 
 end
